8d0983ec7dfb914e7eb61ccbfec354b229873199
shimpei-noda <shimpei.noda@linecorp.com>
2019-08-25 20:47:15 +0900

    Disable flushall

--- index.ts
+++ index.ts
@@ -270,7 +270,7 @@ fastify.get("/", { beforeHandler: fillinUser }, async (request, reply) => {
 });
 
 fastify.get("/initialize", async (_request, reply) => {
-  await redis.flushall();
+  // await redis.flushall();
   await execFile("../../db/init.sh");
   const users = await fastify.mysql.query("SELECT id, nickname FROM users");
   users.forEach(async (user) => {
