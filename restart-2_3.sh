22577893d166c51422c035564cbf2b8e7e1f7285
shimpei-noda <shimpei.noda@linecorp.com>
2019-08-25 17:40:23 +0900

    Added restart scripts

---
+++ restart-2_3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  BRANCH="master"
+else
+  BRANCH="$1"
+fi
+
+echo "Branch: ${BRANCH}"
+
+set -e
+cd /home/ubuntu/nodejs
+
+# force pull
+git fetch origin
+git reset --hard origin/${BRANCH}
+
+# build
+# sudo chmod -R 777 /home/isucon/webapp/react/public/
+# (cd nodejs && npm install && npm cache clean)
+# (cd nodejs && MYSQL_HOST=172.29.2.63 node init.js)
+# (cd react && npm install && npm cache clean && NODE_ENV=production npm run build)
+
+# copy config
+# sudo cp etc/nginx/nginx.conf /etc/nginx/nginx.conf
+# sudo cp etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
+# sudo cp etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+# sudo cp etc/redis/redis.conf /etc/redis/redis.conf
+# sudo cp nodejs/nodejs.service /etc/systemd/system/
+# sudo cp react/react.service /etc/systemd/system/
+
+# restart all service
+# sudo systemctl daemon-reload
+systemctl restart nodejs
+# systemctl restart mysql
+# sudo systemctl restart redis-server
+# sudo systemctl restart react
+# systemctl restart nginx
+
+# echo "" | sudo tee /var/log/mysql/slow.log
+# echo "" | sudo tee /var/log/nginx/access.log
---
+++ restart-all-from-local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xe
+if [ -z "$1" ]; then
+  BRANCH="master"
+else
+  BRANCH="$1"
+fi
+
+scp restart-1.sh root@ec2-3-114-216-227.ap-northeast-1.compute.amazonaws.com:restart.sh
+ssh -A root@ec2-3-114-216-227.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh
+
+scp restart-2_3.sh root@ec2-3-114-227-172.ap-northeast-1.compute.amazonaws.com:restart.sh
+ssh -A root@ec2-3-114-216-227.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh
+
+scp restart-2_3.sh root@ec2-3-114-116-138.ap-northeast-1.compute.amazonaws.com:restart.sh
+ssh -A root@ec2-3-114-216-227.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh
