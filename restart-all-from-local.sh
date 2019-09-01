919a588b4bca8b4228a21f12ba1287597d7e0185
shimpei-noda <shimpei.noda@linecorp.com>
2019-09-01 13:20:23 +0900

    Added branch option to select branch

--- restart-all-from-local.sh
+++ restart-all-from-local.sh
@@ -6,10 +6,10 @@ else
 fi
 
 scp restart-1.sh root@ec2-3-114-216-227.ap-northeast-1.compute.amazonaws.com:restart.sh
-ssh -A root@ec2-3-114-216-227.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh
+ssh -A root@ec2-3-114-216-227.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh $BRANCH
 
 scp restart-2_3.sh root@ec2-3-114-227-172.ap-northeast-1.compute.amazonaws.com:restart.sh
-ssh -A root@ec2-3-114-227-172.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh
+ssh -A root@ec2-3-114-227-172.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh $BRANCH
 
 scp restart-2_3.sh root@ec2-3-114-116-138.ap-northeast-1.compute.amazonaws.com:restart.sh
-ssh -A root@ec2-3-114-116-138.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh
+ssh -A root@ec2-3-114-116-138.ap-northeast-1.compute.amazonaws.com /bin/bash restart.sh $BRANCH
