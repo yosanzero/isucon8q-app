d3e2ba87eec91d9231550911292b6e7f13fff216
root <root@ip-192-168-0-211.ap-northeast-1.compute.internal>
2019-09-02 15:02:00 +0900

    Enable to tracer analytics

--- tracer.ts
+++ tracer.ts
@@ -1,3 +1,5 @@
 import tracer from "dd-trace";
-tracer.init(); // initialized in a different file to avoid hoisting.
+tracer.init({
+  analytics: true
+}); // initialized in a different file to avoid hoisting.
 export default tracer;
